ab445399a1d3355bd532510f975c7fc49bbe890d
robert.ababei <robert.ababei9@gmail.com>
2023-07-19 15:56:50 +0300

    Added antd & tailwind

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -7,10 +7,32 @@ export default function Login() {
 
 
     return (
-        <div>
-            <audio controls autoPlay loop  id="playAudio">
-                <source src={wotlkAudio} />
-            </audio>
+        <div className='flex flex-col justify-center items-center bg-red-200 h-full'>
+
+            <h1 className='text-3xl font-bold underline'>
+                Hello, this is Login page
+            </h1>
+
+            <h1 className='text-xl font-semibold '>
+                You will learn step by step to use tailwind
+            </h1>
+
+            <h1 className='text-xl font-semibold '>
+                Tailwind it's a framework over CSS. It's like a wrapper over any CSS functionality
+            </h1>
+
+            <h1 className='text-xl font-semibold '>
+                The advantages of CSS it's that you can stylize everything very fast
+            </h1>
+
+            <h1 className='text-xl font-semibold '>
+                Here it's the
+                <a target='_blank' className='underline text-blue-800' href='https://tailwindcss.com/docs/flex-direction'>
+                    tailwind documentation
+                </a>. 
+                You will need to look for the properties you want to use.
+                Especially at the begining. 
+            </h1>
         </div>
     )
 }
