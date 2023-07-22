1933ea8adb17354eea8c174943fce5f83d0ad568
Razvan-desktop <razvanababei@yahoo.com>
2023-07-22 21:26:48 +0300

    Updated Login page using Ant Design

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import { Input } from 'antd';
+import { Checkbox, Form, Input } from 'antd';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import Button from '../components/_shared/Button/Button';
 import humansImage from '../assets/images/humans.png';
 
@@ -9,6 +10,10 @@ export default function Login() {
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
 
+    const onFinish = (values: { email: string; password: string }) => {
+        console.log('Received values of form:', values);
+//Useful for API call to authenticate the user
+    }
 
     return (
         <div className='flex flex-col justify-center items-center  h-full'>
@@ -16,8 +21,8 @@ export default function Login() {
             <div className='flex w-100 max-w-[1014px] bg-white rounded-3xl p-5'>
                 <div className='w-[60%]'>
                     <img
-                        src={humansImage} 
-                        alt="Humans image" 
+                        src={humansImage}
+                        alt="Humans image"
                     />
                 </div>
                 <div className='w-[40%] flex flex-col justify-center items-center'>
@@ -26,24 +31,73 @@ export default function Login() {
                         <h4 className='text-gray-400 '>Enter your email and password to sign in !</h4>
                     </div>
 
-                    <Input 
-                        className='mb-3' 
-                        placeholder='Email'
-                        value={email}
-                        onChange={(e: React.FormEvent<HTMLInputElement>) => setEmail(e.currentTarget.value)}
-                    />
-                    <Input.Password 
-                        placeholder='Password' 
-                        value={password}
-                        onChange={(e: React.FormEvent<HTMLInputElement>) => setPassword(e.currentTarget.value)}
-                    />
+                    <Form
+                        name="login-form"
+                        className='mb-3'
+                        initialValues={{
+                            email: '',
+                            password: '',
+                        }}
+                    >
+                        <Form.Item
+                            name='email'
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please enter your email!',
+                                },
+                                {
+                                    type: 'email',
+                                    message: 'Please enter a valid email address!',
+                                },
+                            ]}
+                        >
+                            <Input
+                                prefix={<UserOutlined />}
+                                placeholder='Email'
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </Form.Item>
 
-                    <Button
-                        className='mt-10 w-full' 
-                        type='primary' 
-                        onClick={() => console.log("I'm clicked")}>
+                        <Form.Item
+                            name="password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please enter your password!',
+                                },
+                            ]}
+                        >
+                            <Input.Password
+                                prefix={<LockOutlined />}
+                                placeholder='Password'
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </Form.Item>
+
+                        <Form.Item>
+                            <Form.Item
+                                name="remember"
+                                valuePropName='checked'
+                                noStyle
+                            >
+                                <Checkbox>Remember me</Checkbox>
+                            </Form.Item>
+                            <a className="login-form-forgot" href="">
+                                Forgot your password?
+                            </a>
+                        </Form.Item>
+
+
+                        <Button
+                            className='mt-10 w-full'
+                            type='primary'
+                            onClick={() => console.log("I'm clicked")}>
                             SIGN IN
-                    </Button>
+                        </Button>
+                    </Form>
 
                 </div>
             </div>
