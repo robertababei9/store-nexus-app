3f333b1c3b0ce9ce4d839025c6b3baf8b6a2c565
Razvan-desktop <razvanababei@yahoo.com>
2023-08-02 19:56:11 +0300

    Added logo+collapse button

--- src/components/dashboard/Menu.tsx
+++ src/components/dashboard/Menu.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import humansImage from '../../assets/images/humans.png';
+
 import {
   SettingOutlined,
   ShopOutlined,
   UserOutlined,
   DashboardOutlined,
-  LoginOutlined 
+  LoginOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined
 } from '@ant-design/icons';
-import { Menu } from 'antd';
+import { Menu, Layout, Button } from 'antd';
 import { useNavigate } from 'react-router-dom';
 
+const { Header, Sider } = Layout;
+
 type MenuItem = {
-  key: string; // Adaugă tipul pentru cheie aici
+  key: string;
   icon?: React.ReactNode;
   children?: MenuItem[];
   label: React.ReactNode;
@@ -38,16 +44,12 @@ const items: MenuItem[] = [
   getItem('Stores', '/stores', <ShopOutlined />),
   getItem('Users', '/users', <UserOutlined />),
   getItem('Settings', '/settings', <SettingOutlined />),
-  getItem('Log In', '/login',<LoginOutlined />),
-
+  getItem('Log In', '/login', <LoginOutlined />),
 ];
 
-
-
-
 const MenuComponent: React.FC = () => {
-
   const navigate = useNavigate();
+  const [collapsed, setCollapsed] = useState(false);
 
   const handleMenuClick = ({ key }: { key: React.Key }) => {
     if (key) {
@@ -56,16 +58,36 @@ const MenuComponent: React.FC = () => {
   };
 
   return (
- 
+    <Layout>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        {/* Modificarea: Adăugăm un antet pentru imagine și buton */}
+        <Header className="bg-transparent p-0 flex justify-between items-center">
+          {/* Modificarea: Adăugăm imaginea */}
+          <div className='w-[40%]'>
+            <img
+              src={humansImage}
+              alt="Humans image"
+            />
+          </div>
+          {/* Modificarea: Stilizăm butonul cu culoarea textului și a fundalului */}
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            className={`text-xl w-166 h-16 text-white`} 
+            //AICI E PROBLEMA! mereu cand dau hover pe buton i se schimba in negru sau bckgrnd (am stat cv timp sa rezolv si nimic,bft)
+          />
+        </Header>
         <Menu
-          className='w-[220px] shadow-2xl'
-          defaultSelectedKeys={["/dashboard"]}
-          defaultOpenKeys={['sub1']}
+          theme="dark"
           mode="inline"
-          theme="light"
+          defaultSelectedKeys={['/dashboard']}
+          defaultOpenKeys={['/dashboard']}
           items={items}
           onClick={handleMenuClick}
         />
+      </Sider>
+    </Layout>
   );
 };
 
