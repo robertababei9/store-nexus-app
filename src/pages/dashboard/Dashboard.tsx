b39d6b336741a322bd78cecf7dca6ae4bcdb28e7
Razvan-desktop <razvanababei@yahoo.com>
2023-08-02 16:40:40 +0300

    small changes

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -2,10 +2,17 @@ import React from 'react'
 import { Typography  } from 'antd';
 
 import SalesAreaChart from '../../components/dashboard/SalesAreaChart';
-import Menu from '../../components/dashboard/Menu';
+import Menu from '../../components/dashboard/Menu'; // asta nu vine in /dashboard ... ca noi sa o sa il refolosim peste tot ---> _shared
 
 const { Title } = Typography;
 
+// te verific acum , ia sa mai vedem ce gasim
+// ok, E OK ASA MOMENTAN, normal ca e, perfect chiar
+// se poate sterge branch-ul asta nou facut de mn prost? si fac altu bun :)
+// dar lasa asa momentan, e ok
+// nu mai gresesti data viitoare
+// dar acum hai sa iei modificarile de pe main si sa le pui la tn
+//hai - pai hai
 export default function Dashboard() {
 
 
@@ -13,7 +20,7 @@ export default function Dashboard() {
   return (
     <div className='flex h-screen w-64 bg-dark'>
     <Menu />
-   <div className='flex space-between justify-start items-start sm:px-32 px-4 sm:py-8 py-6'>
+   <div className='flex flex-col justify-start items-start sm:px-32 px-4 sm:py-8 py-6'>
       
       <Title>Dashboard</Title>
 
