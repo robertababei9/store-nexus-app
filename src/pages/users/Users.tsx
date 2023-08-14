5dd72059a95102f7c77171b177b6c530bd9b4e67
Razvan-desktop <razvanababei@yahoo.com>
2023-08-14 14:54:23 +0300

    WIP: repared Users page (table working now)

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Table, Input, Space, Pagination } from 'antd';
+import { Table, Input, Space } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
 import { SearchOutlined } from '@ant-design/icons';
 
@@ -8,29 +8,29 @@ interface DataType {
     name: string;
     email: string;
     location: string;
-    phone: number;
+    phone: string;
     signedup: string;
 }
 
 const columns: ColumnsType<DataType> = [
     {
-        title: 'Name',
+        title: 'NAME',
         dataIndex: 'name',
     },
     {
-        title: 'Email',
+        title: 'EMAIL',
         dataIndex: 'email',
     },
     {
-        title: 'Location',
+        title: 'LOCATION',
         dataIndex: 'location',
     },
     {
-        title: 'Phone',
+        title: 'PHONE',
         dataIndex: 'phone',
     },
     {
-        title: 'Signed up',
+        title: 'SIGNED UP',
         dataIndex: 'signedup',
     },
 ];
@@ -38,250 +38,186 @@ const columns: ColumnsType<DataType> = [
 const data: DataType[] = [
     {
         key: '1',
-        name: '111',
-        email: "Vegetables",
+        name: 'Razvan Ababei',
+        email: "razvanababei@yahoo.com",
         location: "15 Jul 2022",
-        phone: 16000,
+        phone: "08593",
         signedup: "asa"
     },
     {
         key: '2',
-        name: '111',
-        email: "Vegetables",
+        name: 'Robert Ababei',
+        email: "robertababei@yahoo.com",
         location: "15 Jul 2022",
-        phone: 16000,
+        phone: "091234",
         signedup: "asa"
     },
     {
         key: '3',
-        name: '222',
-        email: "anduc",
+        name: 'Dinu Ababei',
+        email: "dinuababei@yahoo.com",
         location: "15 Jul 2022",
-        phone: 16000,
+        phone: "16000",
         signedup: "asa"
     },
     {
         key: '4',
-        name: '222',
-        email: "anduc",
+        name: 'Codrin Ababei',
+        email: "codrinababei@yahoo.com",
         location: "15 Jul 2022",
-        phone: 16000,
+        phone: "16000",
         signedup: "asa"
     },
     {
         key: '5',
-        name: '333',
-        email: "anduc",
+        name: 'Dana Ababei',
+        email: "danaababei@yahoo.com",
         location: "15 Jul 2022",
-        phone: 16000,
+        phone: "16000",
         signedup: "asa"
     },
     {
         key: '6',
-        name: '333',
-        email: "anduc",
+        name: 'Sorin Ababei',
+        email: "sorinababei@yahoo.com",
         location: "15 Jul 2022",
-        phone: 16000,
+        phone: "000009",
         signedup: "asa"
     },
     {
         key: '7',
-        name: '444',
-        email: "anduc",
+        name: 'Dorin Ababei',
+        email: "dorinababei@yahoo.com",
         location: "15 Jul 2022",
-        phone: 16000,
+        phone: "16000",
         signedup: "asa"
     },
     {
         key: '8',
-        name: '444',
-        email: "anduc",
+        name: 'Lili Ababei',
+        email: "liliababei@yahoo.com",
         location: "15 Jul 2022",
-        phone: 16000,
+        phone: "16000",
         signedup: "asa"
     },
     {
         key: '9',
-        name: '555',
-        email: "anduc",
+        name: 'Eliza Giuborunca',
+        email: "elizagiub@yahoo.com",
         location: "15 Jul 2022",
-        phone: 16000,
+        phone: "16000",
         signedup: "asa"
     },
     {
         key: '10',
-        name: '555',
-        email: "anduc",
+        name: 'Tiberiu Giuborunca',
+        email: "tibigiub@yahoo.com",
         location: "15 Jul 2022",
-        phone: 16000,
+        phone: "16000",
         signedup: "asa"
     },
     {
         key: '11',
-        name: '666',
-        email: "anduc",
-        location: "15 Jul 2022",
-        phone: 16000,
+        name: 'Bunicu',
+        email: "bunica@yahoo.com",
+        location: "Vorona",
+        phone: "16000",
         signedup: "asa"
     },
     {
         key: '12',
-        name: '666',
-        email: "anduc",
-        location: "15 Jul 2022",
-        phone: 16000,
-        signedup: "asa"
+        name: 'Bunica',
+        email: "bunicu@yahoo.com",
+        location: "Vorona",
+        phone: "112",
+        signedup: "13/03/2023"
     },
     {
         key: '13',
-        name: '777',
-        email: "anduc",
-        location: "15 Jul 2022",
-        phone: 16000,
-        signedup: "asa"
+        name: 'Matusa',
+        email: "matusa1@yahoo.com",
+        location: "Botosani",
+        phone: "123",
+        signedup: "10/01/2022"
     },
     {
         key: '14',
-        name: '777',
-        email: "anduc",
+        name: 'Alta Matusa',
+        email: "altamatusa@yahoo.com",
         location: "15 Jul 2022",
-        phone: 16000,
-        signedup: "asa"
+        phone: "976321",
+        signedup: "02/02/2020"
     }, {
         key: '15',
-        name: '888',
-        email: "anduc",
+        name: 'Unchiu',
+        email: "unchiugiub@yahoo.com",
         location: "15 Jul 2022",
-        phone: 16000,
-        signedup: "asa"
+        phone: "0725321",
+        signedup: "15/09/2020"
     },
     {
         key: '16',
-        name: '888',
-        email: "anduc",
+        name: 'Sorina',
+        email: "sorinagiub@yahoo.com",
         location: "15 Jul 2022",
-        phone: 16000,
-        signedup: "asa"
+        phone: "0749674772",
+        signedup: "12/05/2021"
     },
     {
         key: '17',
-        name: '999',
-        email: "anduc",
+        name: 'Nu mai am nume',
+        email: "da@yahoo.com",
         location: "15 Jul 2023",
-        phone: 16000,
-        signedup: "asa"
-    },
-    {
-        key: '18',
-        name: '999',
-        email: "anduc",
-        location: "15 Jul 2022",
-        phone: 16000,
-        signedup: "asa"
-    },
-    {
-        key: '19',
-        name: '000',
-        email: "anduc",
-        location: "15 Jul 2022",
-        phone: 16000,
-        signedup: "asa"
-    },
-
-
-
+        phone: "074815231",
+        signedup: "12/05/2020"
+    }
 ];
 
 export default function Users() {
-    const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
-    const [searchText, setSearchText] = useState('');
-    const [currentPage, setCurrentPage] = useState<number>(1);
-    const pageSize = 5;
-  
-    const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
-      setSelectedRowKeys(newSelectedRowKeys);
-    };
-  
-    const rowSelection = {
-      selectedRowKeys,
-      onChange: onSelectChange,
-    };
-  
-    const filteredData = data.filter((item) =>
-      Object.values(item).some(
-        (value) =>
-          value &&
-          typeof value === 'string' &&
-          value.toLowerCase().includes(searchText.toLowerCase())
-      )
-    );
-  
-    const updatePaginatedData = (page: number) => {
-      const startIndex = (page - 1) * pageSize;
-      const endIndex = startIndex + pageSize;
-      return filteredData.slice(startIndex, endIndex);
-    };
-  
-    const paginatedData = updatePaginatedData(currentPage);
-  
-    const onShowSizeChange = (current: number, size: number) => {
-      const newCurrentPage = Math.ceil((current * pageSize) / size);
-      setCurrentPage(newCurrentPage);
-    };
-  
-    const paginationConfig = {
-      current: currentPage,
-      pageSize: pageSize,
-      total: filteredData.length,
-      showTotal: (total: number, range: [number, number]) =>
-        `${range[0]}-${range[1]} of ${total} items`,
-      showSizeChanger: true,
-      pageSizeOptions: ['5', '10'],
-      onChange: (page: number) => {
-        setCurrentPage(page);
-      },
-      onShowSizeChange: onShowSizeChange,
-    };
-  
-    return (
-      <div className='w-full h-full overflow-y-auto'>
-        <div className='relative w-full h-full flex flex-col items-center sm:px-16 px-4 sm:py-8 py-6 z-30'>
-          <h1 className='text-2xl font-bold mb-4'>Users</h1>
-          <Space direction='vertical' style={{ marginBottom: 16 }}>
-            <Input
-              placeholder='Search user'
-              prefix={<SearchOutlined />}
-              value={searchText}
-              onChange={(e) => setSearchText(e.target.value)}
-            />
-          </Space>
-          <Table
-            rowSelection={rowSelection}
-            columns={columns}
-            dataSource={paginatedData}
-            pagination={false}
-            style={{ width: '100%', padding: '24px' }}
+  const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+  const [searchText, setSearchText] = useState('');
 
-          />
-          <Pagination
-            current={currentPage}
-            pageSize={paginationConfig.pageSize}
-            total={filteredData.length}
-            showTotal={paginationConfig.showTotal}
-            showSizeChanger={paginationConfig.showSizeChanger}
-            pageSizeOptions={paginationConfig.pageSizeOptions}
-            onChange={(page: number) => {
-              setCurrentPage(page);
-            }}
-            onShowSizeChange={onShowSizeChange}
-            style={{ marginTop: '16px' }}
-          />
-        </div>
-      </div>
-    );
-  }
+  const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
+    setSelectedRowKeys(newSelectedRowKeys);
+  };
 
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: onSelectChange,
+  };
 
-// nu merge search cand esti pe alte pagini, doar pe pag 1
+  const filteredData = data.filter((item) =>
+  Object.values(item).some((value) => {
+    if (typeof value === 'number') {
+      return value.toString().includes(searchText);
+    }
+    if (value && typeof value === 'string') {
+      return value.toLowerCase().includes(searchText.toLowerCase());
+    }
+    return false;
+  })
+);
 
-
+return (
+  <div className='w-full h-full overflow-y-auto'>
+    <div className='relative w-full h-full flex flex-col items-start sm:px-16 px-4 sm:py-8 py-6 z-30'>
+      <h1 className='text-3xl font-bold mb-4' style={{ marginLeft: '24px', padding: '10px' }}>Users</h1>
+      <Space direction='vertical' style={{ marginBottom: 16, marginLeft: '24px' }}>
+        <Input
+          placeholder='Search user'
+          prefix={<SearchOutlined />}
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+          style={{width: '200px'}}
+        />
+      </Space>
+        <Table
+          rowSelection={rowSelection}
+          columns={columns}
+          dataSource={filteredData}
+          style={{ width: '100%', padding: '24px' }}
+        />
+    </div>
+  </div>
+);
+}
